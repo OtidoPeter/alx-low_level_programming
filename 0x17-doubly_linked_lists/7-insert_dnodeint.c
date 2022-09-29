c3d675d70f1e1423d859e9658a8b6bea53955cb7
Otido Peter <peterotido@gmail.com>
2022-09-29 09:16:11 -0700

    function that inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,49 +1,41 @@
 #include "lists.h"
 
 /**
-  * insert_dnodeint_at_index - Inserts a node a given index.
-  * @idx: The index to insert the node.
-  * @n: The value to insert to the new node.
-  * @h: pointer to head pointer.
-  *
-  * Return: Address of the new node NULL otherwise.
-  */
+ * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ *                            list at a given position.
+ * @h: A pointer to the head of the dlistint_t list.
+ * @idx: The position to insert the new node.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i = 0;
-
-	dlistint_t *tmp, *tmp1;
-
-	if (h == NULL)
-		return (NULL);
+	dlistint_t *tmp = *h, *new;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	tmp1 = *h;
-
-	for (; tmp1 != NULL && i < idx;)
+	for (; idx != 1; idx--)
 	{
-		tmp1 = tmp1->next;
-		i++;
-	}
-
-	if (tmp1 == NULL && i == idx)
-		return (add_dnodeint_end(h, n));
-
-	else if (tmp1 != NULL)
-	{
-		tmp = malloc(sizeof(dlistint_t));
-
+		tmp = tmp->next;
 		if (tmp == NULL)
 			return (NULL);
-		tmp->n = n;
-		tmp1->prev->next = tmp1;
-		tmp->prev = tmp1->prev;
-		tmp1->prev = tmp;
-		tmp->next = tmp1;
-
-		return (tmp);
 	}
-	return (NULL);
+
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
 }
